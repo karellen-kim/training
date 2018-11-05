bf745c759e4a1d4bb926ddaa241a9221d988b666
karellen <karellen.kim@daumkakao.com>
2018-11-05 19:52:26 +0900

    1.4 회문의 순열인지 확인하는 함수

--- src/algorithm/array.py
+++ src/algorithm/array.py
@@ -1,8 +1,10 @@
 import math
 
+print("""
 ###################################
 ##  1.2 같은 순열인지 확인하는 함수
 ###################################
+""")
 def hasSameChars(a, b) :
     listA = dic_v2(a)
     listB = dic_v2(b)
@@ -26,9 +28,11 @@ def dic_v2(str) :
 print(hasSameChars("abba", "baaa"))
 print(hasSameChars("abba", "baacca"))
 
+print("""
 ###################################
 ## 1.3 모든 공백을 %20로 변경하는 함수
 ###################################
+""")
 def replaceSpace(str) :
     list = []
     for ch in str :
@@ -40,9 +44,11 @@ def replaceSpace(str) :
 
 print(replaceSpace("Hello   World!"))
 
+print("""
 ###################################
 ## 1.4 회문의 순열인지 확인하는 함수
 ###################################
+""")
 ## O(n^2)
 def isPalindrome(str) :
     map = {}
@@ -50,23 +56,43 @@ def isPalindrome(str) :
         if (map.get(ch) is None) : map[ch] = 1
         else : map[ch] = map.get(ch) + 1
 
-    oddCount = 0
     for k in map.keys() :
         if (map.get(k) % 2 != 0) :
-            if (oddCount > 0) : return False
-            else : oddCount += 1
+            return False
     return True
 
-print(isPalindrome("aaaaoo"))
-print(isPalindrome("aaacccoo"))
+## 성능을 높여보자
+def isPalindrome_v2(str) :
+    array = [0] * getCharIndex('z')
+    for ch in str :
+        array[getCharIndex(ch)] = array[getCharIndex(ch)] + 1
 
+    for k in array :
+        if k % 2 != 0 :
+            return False
+    return True
+
+## for 문을 줄여보자
+def isPalindrome_v3(str) :
+    odd = 0
+    array = [0] * getCharIndex('z')
+    for ch in str :
+        array[getCharIndex(ch)] = array[getCharIndex(ch)] + 1
+        if array[getCharIndex(ch)] % 2 != 0 :
+            odd = odd + 1
+        else :
+            odd = odd - 1
+    return odd <= 1
+
+def getCharIndex(ch) :
+    return ord(ch) - ord('a')
+
+print(isPalindrome_v3("aaaaoo"))
+print(isPalindrome_v3("aaacccoo"))
+
+print("""
 ###################################
 ## 1.5 수정 횟수가 1회 이내인지 확인하는 함수
 ###################################
-def getModifyCount(str1, str2) :
-    idx1 = 0
-    idx2 = 0
-    if (str1[idx1] == str2[idx2]) :
-        idx1 += 1
-        idx2 += 1
-
+""")
+def diff(str1, str2)
