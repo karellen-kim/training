85f9a9f3e533dcd64eea02b581f698232a080288
karellen-kim <vies00@hanmail.net>
2019-01-04 02:01:12 +0900

    2.6 주어진 연결 리스트가 회문인지 확인하는 함수

--- src/algorithm/linked-list.py
+++ src/algorithm/linked-list.py
@@ -1,21 +1,7 @@
-import inspect
+import math
+import utils
 
-def __Title__(title) :
-    print("\n")
-    print("###################################")
-    print("## ", title)
-    print("###################################")
-
-def __print__(var) :
-    callers_local_vars = inspect.currentframe().f_back.f_locals.items()
-    name = [var_name for var_name, var_val in callers_local_vars if var_val is var]
-    if len(name) == 0 :
-        print(var)
-    else :
-        print("%s = %s" % (name[0], str(var)))
-
-
-__Title__("2.1 연결리스트 중복없애기")
+utils.__Title__("2.1 연결리스트 중복없애기")
 class Node :
     def __init__(self, item):
         self.item = item
@@ -68,12 +54,12 @@ def removeDuplicate(list) :
     return list
 
 list = [1,2,3,1,5,6,7,2,1]
-__print__(list)
+utils.__print__(list)
 linkedList = LinkedList(list)
-__print__(removeDuplicate(linkedList)) # O(N)
+utils.__print__(removeDuplicate(linkedList)) # O(N)
 
 
-__Title__("2.2 단방향 연결리스트에서 뒤에서 N번째 원소")
+utils.__Title__("2.2 단방향 연결리스트에서 뒤에서 N번째 원소")
 def getReverseAt(list, n) :
     pre = list.top
     cur = list.top
@@ -97,11 +83,11 @@ def getReverseAt(list, n) :
         return None
 
 list = [1,2,3,1,5,6,7,2,1]
-__print__(list)
+utils.__print__(list)
 linkedList = LinkedList(list)
-__print__(getReverseAt(linkedList, 2)) # O(N)
+utils.__print__(getReverseAt(linkedList, 2)) # O(N)
 
-__Title__("2.3 단방향 연결리스트에서 주어진 노드 삭제 (head 알수 없음)")
+utils.__Title__("2.3 단방향 연결리스트에서 주어진 노드 삭제 (head 알수 없음)")
 def getAt(list, n) :
     cur = list.top
     idx = 1
@@ -122,14 +108,14 @@ def removeNode(node) :
         node.next = next.next
 
 list = [1,2,3,1,5,6,7,2,1]
-__print__(list)
+utils.__print__(list)
 linkedList = LinkedList(list)
 target = getAt(linkedList, 3)
-__print__(target.item)
+utils.__print__(target.item)
 removeNode(target)
-__print__(linkedList) # O(N)
+utils.__print__(linkedList) # O(N)
 
-__Title__("2.5 리스트가 하나의 숫자를 가리킨다고 가정할 때, 두 리스트의 합을 반환하는 리스트")
+utils.__Title__("2.5 리스트가 하나의 숫자를 가리킨다고 가정할 때, 두 리스트의 합을 반환하는 리스트")
 # 7->1->6, 5->9->2 617 + 295 => 912 => 2->1->9
 def sumLists_v1(list1, list2) :
     sum = getNumber(list1) + getNumber(list2)
@@ -152,7 +138,7 @@ def toList(num) :
 list1 = LinkedList([7,1,6])
 list2 = LinkedList([5,9,2])
 sumList = sumLists_v1(list1, list2) # O(8N)
-__print__(sumList)
+utils.__print__(sumList)
 
 def sumLists_v2(list1, list2) :
     n1 = list1.top
@@ -178,14 +164,83 @@ def getNum(node) :
 list1 = LinkedList([7,1,6])
 list2 = LinkedList([5,9,2])
 sumList2 = sumLists_v2(list1, list2)
-__print__(sumList2)
+utils.__print__(sumList2)
 
-__Title__("2.6 주어진 연결 리스트가 회문인지 확인하는 함수")
+utils.__Title__("2.6 주어진 연결 리스트가 회문인지 확인하는 함수")
 # 0->1->2->1->0 => 회문
+def isPalindrome_v1(list) :
+    items = []
 
-__Title__("2.7 값도 위치도 동일한 두 리스트의 교집합 : 마지막 노드가 같아야 함")
+    cur = list.top
+    while cur != None :
+        items.append(cur.item)
+        cur = cur.next
+
+    startIdx = 0
+    endIdx = len(items) - 1
+    while startIdx <= endIdx :
+        if items[startIdx] != items[endIdx] :
+            return False
+        startIdx = startIdx + 1
+        endIdx = endIdx - 1
+    return True
+
+# Doubly linked list인 경우
+def isPalindrome_v2(list) :
+    length = 1
+    last = list.top
+    while last.next != None :
+        length = length + 1
+        last = last.next
+
+    first = list.top
+    count = math.ceil(length / 2)
+    for i in range(0, count) :
+        if first.item != last.item :
+            return False
+        first = first.next
+        last = last.prev
+    return True
+
+# fast runner, slow runner
+def isPalindrome_v3(list) :
+    fast = list.top
+    slow = list.top
+
+    items = []
+    while fast != None and fast.next != None :
+        items.append(slow.item)
+        fast = fast.next.next
+        slow = slow.next
+
+    # 가운데 값 skip
+    if fast != None :
+        slow = slow.next
+
+    idx = len(items) - 1
+    while slow != None :
+        if items[idx] != slow.item :
+            return False
+        idx = idx - 1
+        slow = slow.next
+    return True
+
+list1 = LinkedList([1,2,4,6,4,2,1])
+list2 = LinkedList([1,2,4,4,2,1])
+list3 = LinkedList([1,2,3,4,2,1])
+utils.__print__(isPalindrome_v1(list1)) # O(N)
+utils.__print__(isPalindrome_v1(list2))
+utils.__print__(isPalindrome_v1(list3))
+utils.__print__(isPalindrome_v2(list1)) # O(N)
+utils.__print__(isPalindrome_v2(list2))
+utils.__print__(isPalindrome_v2(list3))
+utils.__print__(isPalindrome_v3(list1)) # O(N)
+utils.__print__(isPalindrome_v3(list2))
+utils.__print__(isPalindrome_v3(list3))
+
+utils.__Title__("2.7 값도 위치도 동일한 두 리스트의 교집합 : 마지막 노드가 같아야 함")
 # 1->2->3, 2->3 교집합 (2->3)
 # 1->2->3, 2->3->1 교집합 없음
 
-__Title__("2.8 순환 연결리스트가 있을 때, 순환되는 첫째 노드는 반환하는 함수")
+utils.__Title__("2.8 순환 연결리스트가 있을 때, 순환되는 첫째 노드는 반환하는 함수")
 # A->B->C->D->E->C(앞의 C와 동일한 객체) => C
---
+++ src/algorithm/stack.py
@@ -0,0 +1,11 @@
+import utils
+
+utils.__Title__("3.1 배열 한개로 스택 3개 구현")
+
+utils.__Title__("3.2 stack에 O(1)시간 안에 동작하는 min함수 구현")
+
+utils.__Title__("3.3 일정 크기로 넘어가면 내부적으로 크기를 분리하는 stack")
+
+utils.__Title__("3.4 스택 2개로 큐 하나를 구현")
+
+utils.__Title__("3.5 추가 스택 1개 이외에 추가 buffer 없이 작은 값 순서대로 스택 정렬하는 프로그램")
