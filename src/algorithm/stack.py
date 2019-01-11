848e819f466fe88bc83ad4240109291926e5816c
karellen-kim <vies00@hanmail.net>
2019-01-12 00:07:18 +0900

    3.1 배열 한개로 스택 3개 구현

--- src/algorithm/stack.py
+++ src/algorithm/stack.py
@@ -1,6 +1,45 @@
 import utils
 
 utils.__Title__("3.1 배열 한개로 스택 3개 구현")
+class FixedSizeStack :
+    def __init__(self, size):
+        self.list = [0 for i in range(0, size * 3)]
+        self.size = size
+        self.pointer = [size * 0, size * 1, size * 2]
+
+    def push(self, num, item):
+        if self.isFull(num) == False:
+            self.list[self.pointer[num]] = item
+            self.pointer[num] = self.pointer[num] + 1
+        else :
+            raise ValueError
+
+    def pop(self, num):
+        if self.isEmpty(num) == False:
+            value = self.list[self.pointer[num]]
+            self.pointer[num] = self.pointer[num] - 1
+            return value
+        else :
+            raise ValueError
+
+    def peek(self, num):
+        return self.list[self.pointer[num]]
+
+    def isEmpty(self, num):
+        return self.size * num == self.pointer[num]
+
+    def isFull(self, num):
+        return self.size * (num + 1) <= self.pointer[num]
+
+fixedSizeStack = FixedSizeStack(3)
+fixedSizeStack.push(0, 1)
+fixedSizeStack.push(0, 2)
+fixedSizeStack.push(0, 3)
+fixedSizeStack.push(1, 1)
+
+#fixedSizeStack.push(0, 1) # Error
+fixedSizeStack.pop(1)
+#fixedSizeStack.pop(1) # Error
 
 utils.__Title__("3.2 stack에 O(1)시간 안에 동작하는 min함수 구현")
 
